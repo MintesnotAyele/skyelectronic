c1f312e923f0b663bb24f5f663a5784c3a506c95
kanu <you@example.com>
2023-05-27 10:53:37 -0900

    updated

---
+++ product.php
@@ -0,0 +1,37 @@
+<?php
+include_once('conn.php');
+$query="select *from product1";
+$result = $conn->query($query);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   
+<table  align"center" border="1px" styles="width:600px; line-height:40px;">
+<tr><th colspan="3"><h1>PRODUCTS TABLE</h1></th></tr>
+<t>
+  <th>ID</th>
+  <th>MODEL</th>
+  <th>QUANTITY</th>
+</t>
+<?php
+while($rows = $result->fetch_assoc())
+{
+?>
+<tr>
+  <td><?php echo $rows['pid'];?> </td>
+  <td><?php echo $rows['model'];?> </td>
+  <td><?php echo $rows['numberofitem'];?> </td>
+</tr>
+<?php
+}
+?>
+  </table> 
+</body>
+</html>
